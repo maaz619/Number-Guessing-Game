387230ad19f4af1fd3ce2b3d3ff9b4bbb14f0c62
maaz619 <ahmedmaaz919@gmail.com>
2020-04-06 21:33:33 +0530

    New

--- guess.py
+++ guess.py
@@ -1,19 +1,27 @@
 import random as rd
-print('For quitting type exit and hit enter!!!\n')
+import math
+print('Guess number between 1 and 1000. You can enter 0 to quit!!!\n')
+answer=rd.randint(1,1001)
+max_guess = math.floor(math.log2(1000))
+no_of_guess = 0
 while True:
     try:
-        answer=rd.randint(1,10)
-        escape='exit'
-        guess=(input('Enter your guess:'))
-        if str(guess)==escape:
+        no_of_guess += 1
+        if no_of_guess > max_guess:
+            print(f'You have exceeded your guess limit!!! Correct answer is {answer}. Thanks for playing')
+            break
+        guess=int(input('Enter your guess:'))
+        if guess == 0:
             print('Thank for playing!!')
             break
-        if 0<int(guess)<11:
-            if int(guess)==answer:
+        if 0<int(guess)<1001:
+            if guess==answer:
                 print("Great job..... You Won!!!!!")
                 break
+            elif guess < answer:
+                print("Number is higher!!!!!")
             else:
-                print('Keep tryin....')
+                print('Number is lower!!!!!')
         else:
             print('Please enter between 1-10')
     except ValueError:
